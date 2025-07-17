26baf391cd1f16084624de8de08d0e8fed3258ee
yamada-ai <hatsudaaa@gmail.com>
2025-07-17 21:43:36 +0900

    socket足した

---
+++ services/work-tracker/app/socket/socket.py
@@ -0,0 +1,20 @@
+# ws_manager.py
+from fastapi import WebSocket
+from typing import List
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, ws: WebSocket):
+        await ws.accept()
+        self.active_connections.append(ws)
+
+    def disconnect(self, ws: WebSocket):
+        self.active_connections.remove(ws)
+
+    async def broadcast(self, message: dict):
+        for ws in self.active_connections:
+            await ws.send_json(message)
+
+ws_manager = ConnectionManager()
